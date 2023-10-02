c9ef28bc9eb430ab1129b409eb80a61e283a1c0d
wang68543 <wang68543@163.com>
2023-10-02 22:42:28 +0800

    同步文件修改

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -2,22 +2,22 @@ package com.itheima.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import com.itheima.reggie.common.R;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/employee")
 public class EmployeeController {
     @Autowired
@@ -56,5 +56,47 @@ public class EmployeeController {
     public R<String> logout(HttpServletRequest request) {
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
+    }
+
+    @ResponseBody
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("新增员工，员工信息 { }", employee.toString());
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+        return R.success("新增员工成功");
+    }
+
+    @ResponseBody
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name) {
+        Page pageInfo = new Page(page, pageSize);
+
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        employeeService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info(employee.toString());
+        Long empId = (Long)request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功");
     }
 }
--- src/main/java/com/itheima/reggie/filter/LoginFilterChecker.java
+++ src/main/java/com/itheima/reggie/filter/LoginFilterChecker.java
@@ -1,6 +1,10 @@
 package com.itheima.reggie.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -11,11 +15,44 @@ import java.io.IOException;
 @Slf4j
 @WebFilter(filterName = "loginFilterCheckFilter", urlPatterns = "/*")
 public class LoginFilterChecker implements Filter {
+    
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String requestURI = request.getRequestURI();
+
+        String[] urls = new String[] {
+          "/employee/login",
+          "/employee/logout",
+          "/backend/**",
+          "/front/**"
+        };
         log.info("拦截网络请求：{}",request.getRequestURI());
-        filterChain.doFilter(request,response);
+        boolean check = check(urls,requestURI);
+        //如果不需要登录
+        if(check) {
+            log.info("本次请求不需要处理:{}",request.getRequestURI());
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //已登录
+        if(request.getSession().getAttribute("employee") != null) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+        response.getWriter().write(JSON.toJSONString(R.error("NOT LOGIN")));
+    }
+
+    public  boolean check(String[] urls, String requestURI) {
+        for (String url: urls) {
+           boolean match = PATH_MATCHER.match(url,requestURI);
+           if(match) {
+               return true;
+           }
+        }
+        return false;
+
     }
 }
